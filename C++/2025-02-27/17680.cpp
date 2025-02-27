72eec005633448e62367882d860a75ebbd7cb391
hwaaaang <68269611+Hwang2442@users.noreply.github.com>
2025-02-27 21:25:54 +0900

    Create 17680.cpp

---
+++ C++/2025-02-27/17680.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_set>
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) 
+{
+	if (cacheSize == 0)
+		return cities.size() * 5;
+
+    int answer = 0;
+
+	queue<string> cacheOrder;
+	unordered_set<string> cache;
+
+	for (int i = 0; i < cities.size(); i++)
+	{
+		string s = cities[i];
+		for (int j = 0; j < s.size(); j++)
+		{
+			s[j] += s[j] >= 'a' ? 0 : 'a' - 'A';
+		}
+
+		if (cache.find(s) != cache.end())
+		{
+			answer++;
+
+			vector<string> temp(cacheOrder.size());
+			while (!cacheOrder.empty())
+			{
+				string a = cacheOrder.front();
+				cacheOrder.pop();
+
+				if (a != s)
+					temp.push_back(a);
+			}
+
+			temp.push_back(s);
+			for (int j = 0; j < temp.size(); j++)
+			{
+				cacheOrder.push(temp[j]);
+			}
+		}
+		else
+		{
+			answer += 5;
+
+			if (cache.size() >= cacheSize)
+			{
+				string remove = cacheOrder.front();
+				cache.erase(remove);
+				cacheOrder.pop();
+			}
+
+			cacheOrder.push(s);
+			cache.insert(s);
+		}
+	}
+
+    return answer;
+}
