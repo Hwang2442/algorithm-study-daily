7f02c10d7193cbb54d51048ec517ee73740ce761
hwaaaang <68269611+Hwang2442@users.noreply.github.com>
2025-07-24 20:59:38 +0900

    Create 154539.cpp

---
+++ C++/2025-07-24/154539.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) 
+{
+    vector<int> answer(numbers.size());
+	stack<int> s;
+	s.push(0);
+
+	for (int i = 1; i < numbers.size(); i++)
+	{
+		while (!s.empty() && numbers[s.top()] < numbers[i])
+		{
+			answer[s.top()] = numbers[i];
+			s.pop();
+		}
+
+		s.push(i);
+	}
+
+	while (!s.empty())
+	{
+		answer[s.top()] = -1;
+		s.pop();
+	}
+
+    return answer;
+}
